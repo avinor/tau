c5b24848d1de09c3b427dd4439ec927a3b01838c
Terje Torkelsen <terje.torkelsen@avinor.no>
2019-08-07 10:44:48 +0200

    Added config composition models, but not used since it is not supported by hcl2 yet

---
+++ pkg/config/comp/doc.go
@@ -0,0 +1,5 @@
+// Package comp contains compositions for configuration structs. These provide common
+// functionality that is shared between the different config elements.
+//
+// hcl2 does not support compositions yet.. so this is for future improvement
+package comp
---
+++ pkg/config/comp/remainer.go
@@ -0,0 +1,35 @@
+package comp
+
+import (
+	"github.com/hashicorp/hcl2/gohcl"
+	"github.com/hashicorp/hcl2/hcl"
+)
+
+// Remainer includes a hcl remain tag to parse all remaining items in block
+type Remainer struct {
+	Config hcl.Body `hcl:",remain"`
+}
+
+// ResolveVariables decodes the config and returns a list of all variables found in hcl.Body.
+// This is used later to be able to determine what variables it needs to resolve before
+// finally resolving the body.
+func (r *Remainer) ResolveVariables() ([]hcl.Traversal, error) {
+	exprs := map[string]hcl.Expression{}
+	diags := gohcl.DecodeBody(r.Config, nil, &exprs)
+
+	if diags.HasErrors() {
+		return nil, diags
+	}
+
+	trav := []hcl.Traversal{}
+	for _, expr := range exprs {
+		vars := expr.Variables()
+		if len(vars) == 0 {
+			continue
+		}
+
+		trav = append(trav, vars...)
+	}
+
+	return trav, nil
+}
